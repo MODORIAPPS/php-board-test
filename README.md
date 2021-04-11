bb02e49fce9d6a1b2d111709188faff9e4e55710
MODORIAPPS <kwonkiseok02@naver.com>
2021-04-11 18:30:42 +0900

    db auto create fixed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 `docker-compose build` <br>
 `docker-compose up -d`
+
+<br>
+if you want to watch all of logs then type below command...<br>
+`docker-compose up`
--- db/setup.sql
+++ db/setup.sql
@@ -6,5 +6,5 @@ CREATE TABLE user(
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     user_name VARCHAR(100) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
-    user_password  VARCHAR(255) NOT NULL,
-)
+    user_password  VARCHAR(255) NOT NULL
+);
